49aa9e940d0f1d8284b0dbfcafe2438bdbca9840
Kevin Castro <kancas.gia@gmail.com>
2020-09-30 20:56:10 -0500

    Catalog menu

--- DecorAR/app/src/main/java/co/edu/unal/decorar/MainActivity.kt
+++ DecorAR/app/src/main/java/co/edu/unal/decorar/MainActivity.kt
@@ -1,9 +1,11 @@
 package co.edu.unal.decorar
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -15,12 +17,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                 R.id.navigation_camera,R.id.navigation_catalog))
+                 R.id.navigation_camera,R.id.navigation_catalog_menu))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- DecorAR/app/src/main/java/co/edu/unal/decorar/ui/catalog/CatalogFragment.kt
+++ DecorAR/app/src/main/java/co/edu/unal/decorar/ui/catalog/CatalogFragment.kt
@@ -4,28 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import co.edu.unal.decorar.R
 
-class CatalogFragment : Fragment() {
-
-    private lateinit var catalogViewModel: CatalogViewModel
-
+class CatalogFragment : Fragment(){
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        catalogViewModel =
-                ViewModelProviders.of(this).get(CatalogViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_catalog, container, false)
-        val textView: TextView = root.findViewById(R.id.text_catalog)
-        catalogViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_catalog, container, false)
     }
+
 }
